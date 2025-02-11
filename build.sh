955f84205a353a96abdfe98ec46d7c42b0dcb0e1
Nathanalex <miarynathan@hotmail.com>
2025-02-11 12:09:24 +0300

    to cosmix

---
+++ build.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+project_name="cosmix"
+
+server_dir="/opt/tomcat/webapps"
+classes_dir="./out/production/Cosmix"
+jar_dir="/Users/macbook/Documents/Java Library"
+
+mkdir -p build
+cp -R "$classes_dir"/* build/
+# cp -R "$web_dir"/* build/
+# cp -R "web/WEB-INF" "build/WEB-INF/"
+# cp -R "lib" build/
+
+jar cf "$project_name.jar" -C build .
+
+#Dispatch framework jar to java library
+mkdir -p "$jar_dir/$project_name"
+cp "./$project_name.jar" "$jar_dir/$project_name"
+
+#Dispatch framework jar to project test
+cp "./$project_name.jar" "/Users/macbook/Documents/ITU/S5/Framework/Biblio/lib/"
+
+
+# cp "$project_name.war" "$server_dir"
